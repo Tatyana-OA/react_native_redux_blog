62ffd55d3277f5e89f4b0c4ca400e73367f1758d
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-29 14:42:58 +0200

    Initial setup

---
+++ assets/src/screens/indexScreen.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+  <View>
+    <Text>Index Screen</Text>
+    <Text>More Text</Text>
+  </View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
