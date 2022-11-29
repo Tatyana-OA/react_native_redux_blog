6b47ca854206a88e05f2fc6ec7bb9140372c1c1f
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-29 15:35:12 +0200

    Replace useState with useReducer

--- assets/src/context/BlogContext.js
+++ assets/src/context/BlogContext.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 
 //Create context
 //Add provider to the context
@@ -6,14 +6,22 @@ import React, { useState } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "add_blog_post":
+      return [...state, { title: `Title: ${state.length + 1}` }];
+    default:
+      return state;
+  }
+};
+
 export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([]);
+  // destructuring assignment - state, dispatch
+  // useReducer -> first argument - reducer function, second - initial state
+  const [blogPosts, dispatch] = useReducer(blogReducer, []);
 
   const addBlogPost = () => {
-    setBlogPosts((state) => [
-      ...state,
-      { title: `Blog post #${blogPosts.length + 1}` },
-    ]);
+    dispatch({ type: "add_blog_post" });
   };
 
   return (
