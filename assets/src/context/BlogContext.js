135455ebb9ffb5c3125ea9f901fcc6bd97e618f1
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-11-30 15:24:02 +0200

    Delete blog post

--- assets/src/context/BlogContext.js
+++ assets/src/context/BlogContext.js
@@ -6,8 +6,16 @@ import createDataContext from "./createDataContext";
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case "add_blog_post":
-      return [...state, { title: `Title: ${state.length + 1}` }];
+    case "add_blogpost":
+      return [
+        ...state,
+        {
+          id: Math.floor(Math.random() * 9999),
+          title: `Title: ${state.length + 1}`,
+        },
+      ];
+    case "delete_blogpost":
+      return state.filter((blogPost) => blogPost.id !== action.payload);
     default:
       return state;
   }
@@ -15,12 +23,18 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = (dispatch) => {
   return () => {
-    dispatch({ type: "add_blog_post" });
+    dispatch({ type: "add_blogpost" });
+  };
+};
+
+const deleteBlogPost = (dispatch) => {
+  return (id) => {
+    dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost },
+  { addBlogPost, deleteBlogPost },
   []
 );
