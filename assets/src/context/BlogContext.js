9caf366b835b6b003fed4e97e6ce880a03cf1794
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-12-05 15:56:43 +0200

    reusable form for create and edit

--- assets/src/context/BlogContext.js
+++ assets/src/context/BlogContext.js
@@ -11,12 +11,16 @@ const blogReducer = (state, action) => {
         ...state,
         {
           id: Math.floor(Math.random() * 9999),
-          title: `Title: ${action.payload.title}`,
+          title: action.payload.title,
           content: action.payload.content,
         },
       ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
+    case "edit_blogpost":
+      return state.map((blogPost) => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost;
+      });
     default:
       return state;
   }
@@ -34,9 +38,15 @@ const deleteBlogPost = (dispatch) => {
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
+const editBlogPost = (dispatch) => {
+  return (id, title, content, onCompleteCallback) => {
+    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+    onCompleteCallback();
+  };
+};
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   []
 );
--- assets/src/screens/EditScreen.js
+++ assets/src/screens/EditScreen.js
@@ -1,52 +1,23 @@
-import React, { useState, useContext } from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet } from "react-native";
+import BlogPostForm from "../components/BlogPostForm";
 import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context);
+  const { state, editBlogPost } = useContext(Context);
+
   const id = navigation.getParam("id");
   const blogPost = state.find((blogPost) => blogPost.id === id);
-  const [title, setTitle] = useState(blogPost.title);
-  const [content, setContent] = useState(blogPost.content);
+
   return (
-    <View>
-      <Text style={styles.label}>Edit Title:</Text>
-      <TextInput
-        style={styles.input}
-        value={title}
-        onChangeText={(text) => setTitle(text)}
-      />
-      <Text style={styles.label}>Edit Content:</Text>
-      <TextInput
-        style={styles.input}
-        value={content}
-        onChangeText={(text) => setContent(text)}
-      />
-      <Button
-        title="Add Blog Post"
-        onPress={() => {
-          addBlogPost(title, content, () => {
-            navigation.navigate("Index");
-          });
-        }}
-      />
-    </View>
+    <BlogPostForm
+      initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        // return user to previous page
+        editBlogPost(id, title, content, () => navigation.pop());
+      }}
+    />
   );
 };
-const styles = StyleSheet.create({
-  input: {
-    fontSize: 18,
-    borderWidth: 1,
-    borderColor: "black",
-    marginBottom: 15,
-    padding: 5,
-    width: "80%",
-    marginLeft: 5,
-  },
-  label: {
-    fontSize: 20,
-    marginBottom: 5,
-    marginLeft: 5,
-  },
-});
+const styles = StyleSheet.create({});
 export default EditScreen;
