2e947e80f4ddb75b68830c3605d6e94811c12718
Tatyana Asenova <tatyana.asenova@augeocorp.net>
2022-12-07 16:14:11 +0200

    Edit and delete

--- assets/src/context/BlogContext.js
+++ assets/src/context/BlogContext.js
@@ -9,15 +9,6 @@ const blogReducer = (state, action) => {
   switch (action.type) {
     case "get_blogposts":
       return action.payload;
-    case "add_blogpost":
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 9999),
-          title: action.payload.title,
-          content: action.payload.content,
-        },
-      ];
     case "delete_blogpost":
       return state.filter((blogPost) => blogPost.id !== action.payload);
     case "edit_blogpost":
@@ -38,20 +29,21 @@ const getBlogPosts = (dispatch) => {
 
 const addBlogPost = (dispatch) => {
   return async (title, content, onCompleteCallback) => {
-    // dispatch({ type: "add_blogpost", payload: { title, content } });
     await jsonServer.post("/blogposts", { title, content });
     onCompleteCallback();
   };
 };
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`);
     dispatch({ type: "delete_blogpost", payload: id });
   };
 };
 const editBlogPost = (dispatch) => {
-  return (id, title, content, onCompleteCallback) => {
-    dispatch({ type: "edit_blogpost", payload: { id, title, content } });
+  return async (id, title, content, onCompleteCallback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content });
+    dispatch({ type: "edit_blogpost", payload: { title, content } });
     onCompleteCallback();
   };
 };
